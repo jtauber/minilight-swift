b07c9bf48493e208862137f9e52c70ce797cf8de
James Tauber <jtauber@jtauber.com>
2014-06-14 20:56:37 -0400

    Initial Commit

---
+++ Minilight/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Minilight
+//
+//  Created by James Tauber on 6/14/14.
+//  Copyright (c) 2014 James Tauber. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
